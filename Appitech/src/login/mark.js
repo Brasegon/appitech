7f9a7359d9da5b3a33267d2488ee661d54ee3336
Brandon Segers <brandon.segers@outlook.fr>
2021-06-14 17:05:09 +0200

    add datable

--- Appitech/src/login/mark.js
+++ Appitech/src/login/mark.js
@@ -3,6 +3,25 @@ import { View, StyleSheet, ScrollView } from 'react-native';
 import { DataTable } from 'react-native-paper';
 
 export default function Mark() {
+  const [page, setPage] = React.useState(0);
+  const [itemsPerPage, setItemsPerPage] = React.useState(2);
+  const data = [
+    {
+      name: 'Frozen Yogurt',
+      grade: 'A',
+      credit: '6.0',
+    },
+    {
+      name: 'Frozen Yogurt',
+      grade: 'A',
+      credit: '6.0',
+    },
+    {
+      name: 'TEST',
+      grade: 'A',
+      credit: '6.0',
+    },
+  ];
   return (
 
     <View style={styles.container}>
@@ -12,56 +31,26 @@ export default function Mark() {
           <DataTable.Title numeric>Grade</DataTable.Title>
           <DataTable.Title numeric>Crédits</DataTable.Title>
         </DataTable.Header>
-        <ScrollView vertical={true}>
-
-          <DataTable.Row style={styles.rowVariante}>
-            <DataTable.Cell style={{ flex: 3 }}>T6 - Organizational Theory</DataTable.Cell>
-            <DataTable.Cell numeric>A</DataTable.Cell>
-            <DataTable.Cell numeric>3</DataTable.Cell>
-          </DataTable.Row>
-
-          <DataTable.Row style={styles.row}>
-            <DataTable.Cell style={{ flex: 3 }}>T6 - Hub</DataTable.Cell>
-            <DataTable.Cell numeric>-</DataTable.Cell>
-            <DataTable.Cell numeric>0</DataTable.Cell>
-          </DataTable.Row>
-
-          <DataTable.Row style={styles.rowVariante}>
-            <DataTable.Cell style={{ flex: 3 }}>T6 - Web Security</DataTable.Cell>
-            <DataTable.Cell numeric>B</DataTable.Cell>
-            <DataTable.Cell numeric>3</DataTable.Cell>
-          </DataTable.Row>
-
-          <DataTable.Row style={styles.row}>
-            <DataTable.Cell style={{ flex: 3 }}>T6 - PHP Framework & REST API</DataTable.Cell>
-            <DataTable.Cell numeric>A</DataTable.Cell>
-            <DataTable.Cell numeric>3</DataTable.Cell>
-          </DataTable.Row>
-
-          <DataTable.Row style={styles.rowVariante}>
-            <DataTable.Cell style={{ flex: 3 }}>T6 - JS Framework</DataTable.Cell>
-            <DataTable.Cell numeric>A</DataTable.Cell>
-            <DataTable.Cell numeric>3</DataTable.Cell>
-          </DataTable.Row>
-
-          <DataTable.Row style={styles.row}>
-            <DataTable.Cell style={{ flex: 3 }}>T6 - AppDev - Epicture</DataTable.Cell>
-            <DataTable.Cell numeric>A</DataTable.Cell>
-            <DataTable.Cell numeric>3</DataTable.Cell>
-          </DataTable.Row>
-
-          <DataTable.Row style={styles.rowVariante}>
-            <DataTable.Cell style={{ flex: 3 }}>T6 - DevOps</DataTable.Cell>
-            <DataTable.Cell numeric>-</DataTable.Cell>
-            <DataTable.Cell numeric>3</DataTable.Cell>
-          </DataTable.Row>
-
-          <DataTable.Row style={styles.row}>
-            <DataTable.Cell style={{ flex: 3 }}>T6 - AppDev - Epicture</DataTable.Cell>
-            <DataTable.Cell numeric>A</DataTable.Cell>
-            <DataTable.Cell numeric>3</DataTable.Cell>
-          </DataTable.Row>
-        </ScrollView>
+        {data
+            .slice(
+              page * itemsPerPage,
+              page * itemsPerPage + itemsPerPage,
+            )
+            .map(row => (
+              <DataTable.Row>
+                <DataTable.Cell>{row.name}</DataTable.Cell>
+                <DataTable.Cell>{row.grade}</DataTable.Cell>
+                <DataTable.Cell>{row.credit}</DataTable.Cell>
+              </DataTable.Row>
+            ))}
+            <DataTable.Pagination
+              page={page}
+              numberOfPages={Math.ceil(data.length / itemsPerPage)}
+              onPageChange={(page) => setPage(page)}
+              label={page}
+              numberOfItemsPerPage={data.length}
+              setItemsPerPage={setItemsPerPage}
+            />
       </DataTable>
     </View>
   );
