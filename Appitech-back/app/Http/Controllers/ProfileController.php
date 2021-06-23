a616634621f13fcfcfb1eeeb0ad91f19be555ba3
Valounte <valentin.lyon@epitech.eu>
2021-06-23 21:02:05 +0200

    rectification include back

--- Appitech-back/app/Http/Controllers/ProfileController.php
+++ Appitech-back/app/Http/Controllers/ProfileController.php
@@ -3,19 +3,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Utils\EpitechApi;
-
+use Firebase\JWT\JWT;
+use App\Utils\JWT as UtilsJWT;
+use App\Utils\Message;
 
 class ProfileController extends Controller {
     /**
      * Gets the logged user's profile from Epitech API
      *
      */
-    public function getProfile() {
+    public function getProfile(Request $request) {
         $path = "/user/?format=json";
         $jwtData = UtilsJWT::authorize($request);
-        if (is_null($jwtData)) {
+         if (is_null($jwtData)) {
             return Message::createMessage(403, "Pas autorisé");
-        }
+         }
         $data = get_object_vars($jwtData);
         return Message::createMessage(500, EpitechApi::get($path, $data));
     }
--- Appitech-back/routes/api.php
+++ Appitech-back/routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ConnectionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
 
 /*
 |--------------------------------------------------------------------------
