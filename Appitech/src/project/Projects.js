a05a97ddd56988c9cd3d5e82540b7d98ea13351a
Brandon Segers <brandon.segers@outlook.fr>
2021-07-13 10:05:13 +0200

    fix project part

--- Appitech/src/project/Projects.js
+++ Appitech/src/project/Projects.js
@@ -24,21 +24,21 @@ export default class Craigslist extends Component {
       modalVisible: false,
       loading: true,
       userSelected: [],
-       data: [
-      //   { id: 1, name: "T6 - Part-time job", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29', count: 0, register: false},
-      //   { id: 2, name: "T6 - PCP Development", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 1, register: false },
-      //   { id: 3, project: "Noob", start_project: '2021-06-29', end_project: '2021-06-29', name: "T6 - Binary Security", image: "https://img.icons8.com/clouds/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 2, register: true},
-      //   { id: 4, name: "T6 - PHP Framework & REST API", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29', count: 3, register: false},
-      //   { id: 5, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
-      //   { id: 6, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
-      //   { id: 7, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
-      //   { id: 8, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
-      //   { id: 9, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
-      //   { id: 10, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
-       ]
+      data: [
+        //   { id: 1, name: "T6 - Part-time job", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29', count: 0, register: false},
+        //   { id: 2, name: "T6 - PCP Development", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 1, register: false },
+        //   { id: 3, project: "Noob", start_project: '2021-06-29', end_project: '2021-06-29', name: "T6 - Binary Security", image: "https://img.icons8.com/clouds/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 2, register: true},
+        //   { id: 4, name: "T6 - PHP Framework & REST API", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29', count: 3, register: false},
+        //   { id: 5, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
+        //   { id: 6, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
+        //   { id: 7, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
+        //   { id: 8, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
+        //   { id: 9, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
+        //   { id: 10, name: "T6 - Organizational Theory", image: "https://img.icons8.com/bubbles/1000/000000/module.png", start: '2021-06-29', end: '2021-06-29',  count: 3, register: false },
+      ]
     };
   }
-  
+
 
   componentDidMount() {
     this.getInfo();
@@ -51,9 +51,9 @@ export default class Craigslist extends Component {
         data: res.message,
       }
     )
-    this.setState({loading: false});
+    this.setState({ loading: false });
     console.log(this.state.loading, 'loadign');
-    }
+  }
 
   showAlertRegister = (data) => {
     this.setState({
@@ -97,107 +97,104 @@ export default class Craigslist extends Component {
     const { project_end } = this.state;
     const progressStyles = StyleSheet.create({
       containerStyle: {
-      flex: 1,
-      justifyContent: 'space-evenly',
+        flex: 1,
+        justifyContent: 'space-evenly',
       },
-      });
+    });
 
-
-    
-    return (
-      <View style={styles.container}>
+    if (this.state.loading) {
+      return (<View style={styles.container}>
         {this.state.loading &&
-                <AnimatedLoader
-                    visible={this.state.loading}
-                    overlayColor="rgba(255,255,255,0.75)"
-                    source={require("../login/loader.json")}
-                    animationStyle={styles.lottie}
-                    speed={1}
-                />}
+          <AnimatedLoader
+            visible={this.state.loading}
+            overlayColor="rgba(255,255,255,0.75)"
+            source={require("../login/loader.json")}
+            animationStyle={styles.lottie}
+            speed={1}
+          />}</View>)
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.title}><Text style={styles.titleText}>Modules <MaterialCommunityIcons name="calendar-text" color={"#3f72af"} size={30}></MaterialCommunityIcons></Text></View>
+          <AwesomeAlert
+            show={showAlertRegister}
+            showProgress={false}
+            data={this.state.data}
+            title={titre}
+            message="You are Not Registered"
+            closeOnTouchOutside={true}
+            closeOnHardwareBackPress={false}
+            showCancelButton={true}
+            showConfirmButton={true}
+            cancelText="Ok"
+            confirmText="Register Me"
+            confirmButtonColor="#008080"
+            onCancelPressed={() => {
+              this.hideAlertRegister();
+            }}
+            onConfirmPressed={() => {
+              this.hideAlertRegister();
+            }}
+          />
+          <AwesomeAlert
+            show={showAlertModule}
+            showProgress={false}
+            data={this.state.data}
+            title={titre}
+            message={"Project : " + project_titre + "\n\tStart : " + project_start + "\n\tEnd : " + project_end}
+            closeOnTouchOutside={true}
+            closeOnHardwareBackPress={false}
+            showCancelButton={true}
+            showConfirmButton={true}
+            cancelText="Ok"
+            confirmText="Unregister"
+            confirmButtonColor="#800000"
+            onCancelPressed={() => {
+              this.hideAlertModule();
+            }}
+            onConfirmPressed={() => {
+              this.hideAlertModule();
+            }}
+          />
+          <FlatList
+            style={styles.contentList}
+            columnWrapperStyle={styles.listContainer}
+            data={this.state.data}
+            keyExtractor={(item) => {
+              return item.id;
+            }}
+            renderItem={({ item }) => {
+              return (
 
-      {!this.state.loading && this.state.data.length > 0 && <View> 
-        <View style={styles.title}><Text style={styles.titleText}>Modules <MaterialCommunityIcons name="calendar-text" color={"#3f72af"} size={30}></MaterialCommunityIcons></Text></View>
-        <AwesomeAlert
-          show={showAlertRegister}
-          showProgress={false}
-          data={this.state.data}
-          title={titre}
-          message="You are Not Registered"
-          closeOnTouchOutside={true}
-          closeOnHardwareBackPress={false}
-          showCancelButton={true}
-          showConfirmButton={true}
-          cancelText="Ok"
-          confirmText="Register Me"
-          confirmButtonColor="#008080"
-          onCancelPressed={() => {
-            this.hideAlertRegister();
-          }}
-          onConfirmPressed={() => {
-            this.hideAlertRegister();
-          }}
-        />
-        <AwesomeAlert
-          show={showAlertModule}
-          showProgress={false}
-          data={this.state.data}
-          title={titre}
-          message={"Project : "+ project_titre + "\n\tStart : " + project_start + "\n\tEnd : " + project_end}
-          closeOnTouchOutside={true}
-          closeOnHardwareBackPress={false}
-          showCancelButton={true}
-          showConfirmButton={true}
-          cancelText="Ok"
-          confirmText="Unregister"
-          confirmButtonColor="#800000"
-          onCancelPressed={() => {
-            this.hideAlertModule();
-          }}
-          onConfirmPressed={() => {
-            this.hideAlertModule();
-          }}
-        />
-        <FlatList
-          style={styles.contentList}
-          columnWrapperStyle={styles.listContainer}
-          data={this.state.data}
-          keyExtractor={(item) => {
-            return item.id;
-          }}
-          renderItem={({ item }) => {
-            return (
-
-              <TouchableOpacity style={styles.card} onPress={() => { this.clickEventListener(item) }}>
-                <TouchableOpacity onPress={() => { this.showAlertRegister(item); }}>
-                  {item.register && <View><Image style={styles.image} source={{uri: "https://img.icons8.com/clouds/1000/000000/module.png"}}/><Text style={{fontSize:10, color:'#85CFCC', alignSelf:'center'}}>Registered</Text></View>}
-                  {!item.register && <View><Image style={styles.image} source={{uri: "https://img.icons8.com/bubbles/1000/000000/module.png"}}/><Text style={{fontSize:10, color:'#FBCD59', alignSelf:'center'}}>Not Registered</Text></View>}
-                </TouchableOpacity>
-                <View style={styles.cardContent}>
-                  <Text style={styles.name}>{item.name}</Text>
-                  <Text style={styles.count}>{item.count} credit(s)</Text>
-                  <View style={progressStyles.containerStyle}>
-                    <ProgressBarAndroid
-                      style={styles.bar}
-                      styleAttr="Horizontal"
-                      indeterminate={false}
-                      progress={item.advance}
-                      color="#2196F3"
-                    />
-                  </View>
-                  <Text style={styles.calendarStart}>{item.start} <MaterialCommunityIcons name="clock-start" color={"grey"} size={20} /></Text>
-                  <Text style={styles.calendarEnd}>{item.end}</Text>
-                  <TouchableOpacity style={styles.followButton} onPress={() => { this.showAlertModule(item); }}>
-                    <Text style={styles.followButtonText}>Project(s)</Text>
+                <TouchableOpacity style={styles.card} onPress={() => { this.clickEventListener(item) }}>
+                  <TouchableOpacity onPress={() => { this.showAlertRegister(item); }}>
+                    {item.register && <View><Image style={styles.image} source={{ uri: "https://img.icons8.com/clouds/1000/000000/module.png" }} /><Text style={{ fontSize: 10, color: '#85CFCC', alignSelf: 'center' }}>Registered</Text></View>}
+                    {!item.register && <View><Image style={styles.image} source={{ uri: "https://img.icons8.com/bubbles/1000/000000/module.png" }} /><Text style={{ fontSize: 10, color: '#FBCD59', alignSelf: 'center' }}>Not Registered</Text></View>}
                   </TouchableOpacity>
+                  <View style={styles.cardContent}>
+                    <Text style={styles.name}>{item.name}</Text>
+                    <Text style={styles.count}>{item.count} credit(s)</Text>
+                    <View style={progressStyles.containerStyle}>
+                      <ProgressBarAndroid
+                        style={styles.bar}
+                        styleAttr="Horizontal"
+                        indeterminate={false}
+                        progress={item.advance}
+                        color="#2196F3"
+                      />
+                    </View>
+                    <Text style={styles.calendarStart}>{item.start} <MaterialCommunityIcons name="clock-start" color={"grey"} size={20} /></Text>
+                    <Text style={styles.calendarEnd}>{item.end}</Text>
+                    <TouchableOpacity style={styles.followButton} onPress={() => { this.showAlertModule(item); }}>
+                      <Text style={styles.followButtonText}>Project(s)</Text>
+                    </TouchableOpacity>
 
-                </View>
-              </TouchableOpacity>
-            )
-          }} />
-          </View>
-         }
-      </View>
-    );
+                  </View>
+                </TouchableOpacity>
+              )
+            }} />
+        </View>)
+    }
   }
 }
 
@@ -240,8 +237,8 @@ const styles = StyleSheet.create({
     borderRadius: 30,
   },
   time: {
-    left : 70
-  },  
+    left: 70
+  },
   name: {
     fontSize: 18,
     maxWidth: 200,
@@ -252,18 +249,18 @@ const styles = StyleSheet.create({
   },
   bar: {
     paddingTop: 20,
-    marginBottom : -10,
-    width : 180
+    marginBottom: -10,
+    width: 180
   },
   calendarStart: {
-    top : 0,
+    top: 0,
     fontSize: 15,
   },
   calendarEnd: {
-    top : -20,
-    left : 105,
+    top: -20,
+    left: 105,
     fontSize: 15,
-    marginBottom : -15,
+    marginBottom: -15,
   },
 
   count: {
