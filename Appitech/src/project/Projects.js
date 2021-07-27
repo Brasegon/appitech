fb13c0c2066d2d425c32fd0ac8b0d5d2f61525c6
Valounte <valentin.lyon@epitech.eu>
2021-07-27 09:28:59 +0200

    projects

--- Appitech/src/project/Projects.js
+++ Appitech/src/project/Projects.js
@@ -15,6 +15,7 @@ import {
   Alert,
   ScrollView
 } from 'react-native';
+import SpinkitButton from 'react-native-spinkit-button';
 
 export default class Craigslist extends Component {
   constructor(props) {
@@ -23,6 +24,9 @@ export default class Craigslist extends Component {
       showAlertRegister: false,
       modalVisible: false,
       loading: true,
+      titre : "",
+      projects:[],
+      isProject: false,
       userSelected: [],
       tmp: "register",
       data: [
@@ -45,6 +49,9 @@ export default class Craigslist extends Component {
     this.getInfo();
   }
 
+
+
+
   async getInfo() {
     var res = await httpClient('/modules', 'get');
     this.setState(
@@ -53,28 +60,21 @@ export default class Craigslist extends Component {
       }
     )
     this.setState({ loading: false });
-    console.log(this.state.loading, 'loadign');
   }
 
-  showAlertModule = (data, tmp) => {
+  async showAlertModule(data, tmp) {
+    var projects = await httpClient('/modules/projects?codeinstance=' + data.codeinstance + '&scholaryear=' + data.scolaryear + '&codemodule=' + data.code, 'get');
+    console.log(projects.message);
     if (data.register == false) {
-      tmp = "register";
+      tmp = "Register";
     } else {
-      tmp = "unregister";
+      tmp = "Unregister";
     }
     this.setState({
-      showAlertModule: true,
       titre: data.name,
-      project_titre: data.project,
-      project_start: data.start_project,
-      project_end: data.end_project,
-      status: tmp
-    });
-  };
-
-  hideAlertModule = () => {
-    this.setState({
-      showAlertModule: false
+      isProject: true,
+      projects : projects.message,
+      status: tmp,
     });
   };
 
@@ -84,14 +84,21 @@ export default class Craigslist extends Component {
     const { project_titre } = this.state;
     const { project_start } = this.state;
     const { project_end } = this.state;
-    const {status} = this.state;
+    const { status } = this.state;
+    const redirectModule = () => {
+      this.setState(
+        {
+          isProject: false
+        }
+      );
+    };
+
     const progressStyles = StyleSheet.create({
       containerStyle: {
         flex: 1,
         justifyContent: 'space-evenly',
       },
     });
-
     if (this.state.loading) {
       return (<View style={styles.container}>
         {this.state.loading &&
@@ -103,67 +110,158 @@ export default class Craigslist extends Component {
             speed={1}
           />}</View>)
     } else {
-      return (
-        <View style={styles.container}>
-          <View style={styles.title}><Text style={styles.titleText}>Modules <MaterialCommunityIcons name="calendar-text" color={"#3f72af"} size={30}></MaterialCommunityIcons></Text></View>
-          <AwesomeAlert
-            show={showAlertModule}
-            showProgress={false}
-            data={this.state.data}
-            title={titre}
-            message={"Project : " + project_titre + "\n\tStart : " + project_start + "\n\tEnd : " + project_end}
-            closeOnTouchOutside={true}
-            closeOnHardwareBackPress={false}
-            showCancelButton={true}
-            showConfirmButton={true}
-            cancelText="Close"
-            confirmText={status}
-            confirmButtonColor="#2ca9e7"
-            onCancelPressed={() => {
-              this.hideAlertModule();
-            }}
-            onConfirmPressed={() => {
-              this.hideAlertModule();
-            }}
-          />
-          <FlatList
-            style={styles.contentList}
-            columnWrapperStyle={styles.listContainer}
-            data={this.state.data}
-            keyExtractor={(item) => {
-              return item.name + "-" + item.id;
-            }}
-            renderItem={({ item }) => {
-              return (
+      if (this.state.isProject == false) {
+        return (
+          <View style={styles.container}>
+            <View style={styles.title}><Text style={styles.titleText}>Modules <MaterialCommunityIcons name="folder-open-outline" color={"#3f72af"} size={30}></MaterialCommunityIcons></Text></View>
+            <AwesomeAlert
+              show={showAlertModule}
+              showProgress={false}
+              data={this.state.data}
+              title={titre}
+              message={"Project : " + project_titre + "\n\tStart : " + project_start + "\n\tEnd : " + project_end}
+              closeOnTouchOutside={true}
+              closeOnHardwareBackPress={false}
+              showCancelButton={true}
+              showConfirmButton={true}
+              cancelText="Close"
+              confirmText={status}
+              confirmButtonColor="#2ca9e7"
+              onCancelPressed={() => {
+                this.hideAlertModule();
+              }}
+              onConfirmPressed={() => {
+                this.hideAlertModule();
+              }}
+            />
+            <FlatList
+              style={styles.contentList}
+              columnWrapperStyle={styles.listContainer}
+              data={this.state.data}
+              keyExtractor={(item) => {
+                return item.name + "-" + item.id;
+              }}
+              renderItem={({ item }) => {
+                return (
 
-                <View style={styles.card} >
-                  <View>
-                    {item.register && <View><Image style={styles.image} source={{ uri: "https://img.icons8.com/clouds/1000/000000/module.png" }} /><Text style={{ fontSize: 10, color: '#85CFCC', alignSelf: 'center' }}>Registered</Text></View>}
-                    {!item.register && <View><Image style={styles.image} source={{ uri: "https://img.icons8.com/bubbles/1000/000000/module.png" }} /><Text style={{ fontSize: 10, color: '#FBCD59', alignSelf: 'center' }}>Not Registered</Text></View>}
-                  </View>
-                  <View style={styles.cardContent}>
-                    <Text style={styles.name}>{item.name}</Text>
-                    <Text style={styles.count}>{item.count} credit(s)</Text>
-                    <View style={progressStyles.containerStyle}>
-                      <ProgressBarAndroid
-                        style={styles.bar}
-                        styleAttr="Horizontal"
-                        indeterminate={false}
-                        progress={item.advance}
-                        color="#2196F3"
-                      />
+                  <View style={styles.card} >
+                    <View>
+                      {item.register && <View><Image style={styles.image} source={{ uri: "https://img.icons8.com/clouds/1000/000000/module.png" }} /><Text style={{ fontSize: 10, color: '#85CFCC', alignSelf: 'center' }}>Registered</Text></View>}
+                      {!item.register && <View><Image style={styles.image} source={{ uri: "https://img.icons8.com/bubbles/1000/000000/module.png" }} /><Text style={{ fontSize: 10, color: '#FBCD59', alignSelf: 'center' }}>Not Registered</Text></View>}
                     </View>
-                    <Text style={styles.calendarStart}>{item.start} <MaterialCommunityIcons name="clock-start" color={"grey"} size={20} /></Text>
-                    <Text style={styles.calendarEnd}>{item.end}</Text>
-                    <TouchableOpacity style={styles.followButton} onPress={() => { this.showAlertModule(item); }}>
-                      <Text style={styles.followButtonText}>Project(s)</Text>
-                    </TouchableOpacity>
+                    <View style={styles.cardContent}>
+                      <Text style={styles.name}>{item.name}</Text>
+                      <Text style={styles.count}>{item.count} credit(s)</Text>
+                      <View style={progressStyles.containerStyle}>
+                        <ProgressBarAndroid
+                          style={styles.bar}
+                          styleAttr="Horizontal"
+                          indeterminate={false}
+                          progress={item.advance}
+                          color="#2196F3"
+                        />
+                      </View>
+                      <Text style={styles.calendarStart}>{item.start} <MaterialCommunityIcons name="clock-start" color={"grey"} size={20} /></Text>
+                      <Text style={styles.calendarEnd}>{item.end}</Text>
+                      <TouchableOpacity style={styles.followButton} onPress={() => { this.showAlertModule(item); }}>
+                        <Text style={styles.followButtonText}>Project(s)</Text>
+                      </TouchableOpacity>
 
+                    </View>
                   </View>
-                </View>
-              )
-            }} />
-        </View>)
+                )
+              }} />
+
+          </View>)
+      }
+      else {
+        return (
+          <View style={styles.container}>
+           
+            <View style={styles.titleProject}>
+              <View style={{marginBottom:30}}></View>
+            <SpinkitButton
+                        borderRadius={11}
+                        top={20}
+                        onPress={redirectModule}
+                        buttonStyle={styles.button}
+                        label={'Back to modules'}
+                        labelStyle={styles.textButtonStyle}
+                        labelAndTextContainer={styles.labelAndTextContainer}
+                        iconComponent={
+                                <MaterialCommunityIcons name="arrow-left-thick" color={"#3f72af"} size={25} style={{position:'absolute', top : 15}}></MaterialCommunityIcons>
+                        }
+                        size={15}
+                        type={'Bounce'}
+                        color={'#FFFFFF'}
+                        animationDuration={300}
+                    />
+                  <SpinkitButton
+                        borderRadius={11}
+                        top={20}
+                        onPress={redirectModule}
+                        buttonStyle={styles.button}
+                        label={this.state.status}
+                        labelStyle={styles.textButtonStyle1}
+                        labelAndTextContainer={styles.labelAndTextContainer1}
+                        size={15}
+                        type={'Bounce'}
+                        color={'#FFFFFF'}
+                        animationDuration={300}
+                    />
+              <Text style={styles.subTitleTextProject}>Projects in</Text>
+              <View
+              style={{
+                position : 'absolute',
+                borderBottomColor: '#2887CB',
+                borderBottomWidth: 2,
+                left: 200,
+                top : 70,
+                width : 110,
+              }}
+            />
+              <Text style={styles.titleTextProject}>{this.state.titre}</Text>
+
+            </View>
+
+
+          <FlatList
+              style={styles.contentList}
+              columnWrapperStyle={styles.listContainer}
+              data={this.state.projects}
+              keyExtractor={(item) => {
+                return item.title;
+              }}
+              renderItem={({ item }) => {
+                return (
+                  <View style={styles.cardProject} >
+                    <View>
+                      {item.registered && <View><Image style={styles.image} source = {require('../../Asset/modules-icon-2.jpg')} /><Text style={{ fontSize: 10, color: '#85CFCC', alignSelf: 'center' }}>Registered</Text></View>}
+                      {!item.registered && <View><Image style={styles.image} source = {require('../../Asset/modules-icon-6.jpg')} /><Text style={{ fontSize: 10, color: '#FBCD59', alignSelf: 'center' }}>Not Registered</Text></View>}
+                    </View>
+                    <View style={styles.cardContent}>
+                      <Text style={styles.name}>{item.title}</Text>
+                      <View style={progressStyles.containerStyle}>
+                        <ProgressBarAndroid
+                          style={styles.bar}
+                          styleAttr="Horizontal"
+                          indeterminate={false}
+                          progress={item.advance}
+                          color="#2196F3"
+                        />
+                      </View>
+                      <Text style={styles.calendarStart}>{item.start} <MaterialCommunityIcons name="clock-start" color={"grey"} size={20} /></Text>
+                      <Text style={styles.calendarEnd}>{item.end}</Text>
+
+                    </View>
+                  </View>
+                )
+              }} />
+
+
+          </View>
+        );
+      };
     }
   }
 }
@@ -206,6 +304,24 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     borderRadius: 30,
   },
+  cardProject: {
+    shadowColor: '#00000021',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 12,
+
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    backgroundColor: "white",
+    padding: 10,
+    flexDirection: 'row',
+    borderRadius: 10,
+  },
   time: {
     left: 70
   },
@@ -216,6 +332,15 @@ const styles = StyleSheet.create({
     alignSelf: 'auto',
     color: "#3399ff",
     fontWeight: 'bold'
+  },
+  nameProject: {
+    position:'absolute',
+    fontSize: 18,
+    maxWidth: 200,
+    flex: 1,
+    alignSelf: 'auto',
+    color: "#3399ff",
+    fontWeight: 'bold',
   },
   bar: {
     paddingTop: 20,
@@ -259,6 +384,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#f6f9fb',
     height: 70,
 
+  }, titleProject: {
+    backgroundColor: '#f6f9fb',
+    height: 160,
   },
   titleText: {
     fontSize: 25,
@@ -266,4 +394,52 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     top: 18
   },
+  titleTextProject: {
+    position : 'absolute',
+    fontSize: 20,
+    color: '#3f72af',
+    top: 85,
+    width : 200,
+    left : 200,
+  },
+  subTitleTextProject: {
+    position : 'absolute',
+    fontSize: 20,
+    color: '#3f72af',
+    top: 30,
+    width : 200,
+    left : 200,
+  },
+  button: {
+    justifyContent: 'center',
+    marginBottom : 30
+},
+textButtonStyle: {
+    flex : 1,
+    color : "#2887CB",
+    textAlign : 'center',
+    textAlignVertical: 'center',
+    fontSize : 17,
+},
+labelAndTextContainer: {
+    borderWidth: 1,
+    width : 120,
+    height : 60,
+    borderColor: "#2887CB",
+    borderRadius: 40,
+},
+textButtonStyle1: {
+    flex : 1,
+    color : "#FFAA4C",
+    textAlign : 'center',
+    textAlignVertical: 'center',
+    fontSize : 17,
+},
+labelAndTextContainer1: {
+    borderWidth: 1,
+    width : 120,
+    height : 60,
+    borderColor: "#FFAA4C",
+    borderRadius: 40,
+}
 });
